e8fa5985df85fff8513badd85a258e5d3de61ed4
addir abasker <abaskeraddir@campus.technion.ac.il>
2018-12-13 18:51:41 +0200

    added TimeSlot implementation

---
+++ app/src/main/java/com/project/technion/appark/TimeSlot.java
@@ -0,0 +1,37 @@
+package com.project.technion.appark;
+import android.support.annotation.NonNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class TimeSlot {
+    private Calendar start;
+    private Calendar end;
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm",Locale.getDefault());
+
+    private String DateToString(Calendar cal){
+        return sdf.format(cal.getTime());
+    }
+
+    public TimeSlot(Calendar start, Calendar end) {
+        this.start = start;
+        this.end = end;
+    }
+
+
+    @NonNull
+    public String toString() {
+        return "Slot [start = " + DateToString(start) + ", end = " + DateToString(end) + "]";
+    }
+
+
+    public Calendar getStart() {
+        return start;
+    }
+
+
+    public Calendar getEnd() {
+        return end;
+    }
+}
