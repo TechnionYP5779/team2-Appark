e8cfb2c8b5a717c6cb946239b359904f3e460008
Unknown <abaskeraddir@campus.technion.ac.il>
2018-12-14 15:46:27 +0200

    updated LoginActivity button functionality

--- app/src/main/java/com/project/technion/appark/activities/LoginActivity.java
+++ app/src/main/java/com/project/technion/appark/activities/LoginActivity.java
@@ -2,14 +2,13 @@ package com.project.technion.appark.activities;
 
 import com.project.technion.appark.R;
 
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.support.annotation.NonNull;
 import android.widget.EditText;
-import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -52,6 +51,33 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        Button user1 = findViewById(R.id.button_user1);
+        user1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MasterActivity.class);
+                i.putExtra("user_id",1);
+                startActivity(i);
+            }
+        });
+        Button user2 = findViewById(R.id.button_user2);
+        user2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MasterActivity.class);
+                i.putExtra("user_id",2);
+                startActivity(i);
+            }
+        });
+        Button user3 = findViewById(R.id.button_user3);
+        user3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MasterActivity.class);
+                i.putExtra("user_id",3);
+                startActivity(i);
+            }
+        });
     }
 
     private void doLogin() {
