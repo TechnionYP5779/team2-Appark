93040ea84f6d7667aff109b21bf8f08aadebd55c
Tal <talporat@campus.technion.ac.il>
2018-12-14 20:23:36 +0200

    update Logout button text

--- app/src/main/java/com/project/technion/appark/activities/MasterActivity.java
+++ app/src/main/java/com/project/technion/appark/activities/MasterActivity.java
@@ -102,7 +102,7 @@ public class MasterActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
             //meanwhile use this as logout
             mAuth.signOut();
             finish();
