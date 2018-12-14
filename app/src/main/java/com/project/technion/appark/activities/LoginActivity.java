fdaf683e2243680b39809f8d5792fca3cd12c8e4
addir abasker <abaskeraddir@campus.technion.ac.il>
2018-12-14 15:32:26 +0200

    added login with custom login(temp)

--- app/src/main/java/com/project/technion/appark/activities/LoginActivity.java
+++ app/src/main/java/com/project/technion/appark/activities/LoginActivity.java
@@ -1,45 +1,79 @@
 package com.project.technion.appark.activities;
 
-import android.content.Intent;
+import com.project.technion.appark.R;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.project.technion.appark.R;
+import android.support.annotation.NonNull;
+import android.widget.EditText;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mLogoutButton;
+    private EditText mEmailInput;
+    private EditText mPasswordInput;
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button user1 = findViewById(R.id.button_user1);
-        user1.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+        mLoginButton = findViewById(R.id.loginButton);
+        mEmailInput = findViewById(R.id.emailInput);
+        mPasswordInput = findViewById(R.id.passwordInput);
+
+        if (mAuth.getCurrentUser() != null) {
+            setResult(RESULT_OK);
+            finish();
+        }
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, MasterActivity.class);
-                i.putExtra("user_id",1);
-                startActivity(i);
+            public void onClick(View v) {
+                doLogin();
             }
         });
-        Button user2 = findViewById(R.id.button_user2);
-        user2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, MasterActivity.class);
-                i.putExtra("user_id",2);
-                startActivity(i);
-            }
-        });
-        Button user3 = findViewById(R.id.button_user3);
-        user3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, MasterActivity.class);
-                i.putExtra("user_id",3);
-                startActivity(i);
-            }
-        });
+
+    }
+
+    private void doLogin() {
+        mAuth.signInWithEmailAndPassword(mEmailInput.getText().toString(), mPasswordInput.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            switchToMainActivity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            LoginActivity.this.setResult(RESULT_CANCELED);
+                            finish();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void switchToMainActivity() {
+        setResult(RESULT_OK);
+        finish();
     }
 }
