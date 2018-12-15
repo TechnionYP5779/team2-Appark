89c33748294b1fe6e98820844083d66c682b5fa3
Unknown <abaskeraddir@campus.technion.ac.il>
2018-12-15 16:40:56 +0200

    Easy on the eyes :D

--- app/src/main/java/com/project/technion/appark/activities/AddParkingSpotActivity.java
+++ app/src/main/java/com/project/technion/appark/activities/AddParkingSpotActivity.java
@@ -1,22 +1,16 @@
 package com.project.technion.appark.activities;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,38 +19,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.project.technion.appark.DataBase;
-import com.project.technion.appark.DummyDB;
 import com.project.technion.appark.ParkingSpot;
 import com.project.technion.appark.R;
-import com.project.technion.appark.TimeSlot;
 import com.project.technion.appark.User;
-
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 public class AddParkingSpotActivity extends AppCompatActivity {
     private static final String TAG = "AddParkingSpotActivity";
-
-    private DataBase db;
     private ParkingSpot mParkingSpot;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
-
-    private Button pick_start, pick_end;
-    private TextView text_start, text_end;
-    private int dayStart, monthStart, yearStart, hourStart, minuteStart;
-    private int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
-    private boolean startTimeWasSet, finishTimeWasSet;
     private EditText etPrice, etAddress;
     private DatabaseReference mDB;
-
-    Calendar calendarStart, calendarFinish;
-
-    private boolean isViewEmpty(TextView view) {
-        return view.getText().toString().isEmpty();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,7 +60,7 @@ public class AddParkingSpotActivity extends AppCompatActivity {
                     Address found_address = list.get(0);
                     double lat = found_address.getLatitude();
                     double lng = found_address.getLongitude();
-                    Log.d("AddParkingSpotActivity","this is what i want" + found_address.getAddressLine(0));
+                    Log.d("AddParkingSpotActivity","Full Address Data: " + found_address.toString());
                     mParkingSpot = new ParkingSpot(mUser.getUid(), price, found_address.getAddressLine(0), lat, lng);
                     mDB.child("Users").child(mUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
