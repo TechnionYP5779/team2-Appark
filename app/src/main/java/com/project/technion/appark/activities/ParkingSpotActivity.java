128d75436c3daaff14cb31805ec834cc175e0925
Unknown <abaskeraddir@campus.technion.ac.il>
2018-12-15 21:24:23 +0200

    clean up

--- app/src/main/java/com/project/technion/appark/activities/ParkingSpotActivity.java
+++ app/src/main/java/com/project/technion/appark/activities/ParkingSpotActivity.java
@@ -1,18 +1,14 @@
 package com.project.technion.appark.activities;
 
-import android.app.AlertDialog;
 import android.support.annotation.NonNull;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,33 +18,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.project.technion.appark.Offer;
 import com.project.technion.appark.ParkingSpot;
-import android.view.View;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.project.technion.appark.R;
 import com.project.technion.appark.User;
-import com.project.technion.appark.adapters.ParkingSpotsAdapter;
 import com.project.technion.appark.adapters.ParkingSpotsOfferAdapter;
-import com.project.technion.appark.TimeSlot;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
-import android.text.format.DateFormat;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
-import com.project.technion.appark.ParkingSpot;
-import com.project.technion.appark.User;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -62,23 +34,7 @@ public class ParkingSpotActivity extends AppCompatActivity {
     private ParkingSpotsOfferAdapter mAdapter;
     private Integer parkingSpotIndex;
     private ParkingSpot parkingSpot;
-
-    private ParkingSpot mParkingSpot;
-
-    private Button pick_start, pick_end;
-    private TextView text_start, text_end;
-    private int dayStart, monthStart, yearStart, hourStart, minuteStart;
-    private int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
-    private boolean startTimeWasSet, finishTimeWasSet;
-
-    Calendar calendarStart, calendarFinish;
-
     private FloatingActionButton mFab;
-
-    private boolean isViewEmpty(TextView view) {
-        return view.getText().toString().isEmpty();
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,8 +68,6 @@ public class ParkingSpotActivity extends AppCompatActivity {
                 } else {
                     noOffers.setVisibility(View.INVISIBLE);
                 }
-
-//                Toast.makeText(MasterActivity.this, u.getName() +" "+u.getContactInfo(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -127,30 +81,7 @@ public class ParkingSpotActivity extends AppCompatActivity {
             Intent i = new Intent(getApplicationContext(), OfferPopActivity.class);
             i.putExtra("parking_spot_index", parkingSpotIndex);
             startActivity(i);
-            /*
-            Log.d(TAG, dayStart + " " + monthStart + " " + yearStart + " " + hourStart + " " + minuteStart);
-            Log.d(TAG, dayFinal + " " + monthFinal + " " + yearFinal + " " + hourFinal + " " + minuteFinal);
-            mDatabaseReference.child("Users").child(mUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    User u = dataSnapshot.getValue(User.class);
-                    ParkingSpot p = u.parkingSpots.get(parkingSpotIndex);
-                    String offerId = mDatabaseReference.push().getKey();
-
-                    mDatabaseReference.child("Offers").child(offerId).setValue(new Offer(p.id,mUser.getUid(),start_time,end_time));
-                    p.offers.add(offerId);
-                    mDatabaseReference.child("Users").child(mUser.getUid()).setValue(u);
-                }
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                }
-            });
-            Toast.makeText(ParkingSpotActivity.this, "the offer was published", Toast.LENGTH_SHORT).show();
-            finish();
-            */
         });
-        //addStartTime();
-        //addFinishTime();
     }
 }
 
