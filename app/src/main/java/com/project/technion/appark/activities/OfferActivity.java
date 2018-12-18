a1ff60ab83883adfc85aee81be483dbde449a5d5
Unknown <abaskeraddir@campus.technion.ac.il>
2018-12-18 11:32:12 +0200

    Working on OfferActivity Design xml - issue #32

---
+++ app/src/main/java/com/project/technion/appark/activities/OfferActivity.java
@@ -0,0 +1,25 @@
+package com.project.technion.appark.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.project.technion.appark.R;
+
+public class OfferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+}
