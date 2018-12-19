a04efd4d1ed81ccf2bef0ec5d31c9cb2005529fc
Unknown <abaskeraddir@campus.technion.ac.il>
2018-12-19 12:06:38 +0200

    Changed order in offerActivity

--- app/src/main/java/com/project/technion/appark/activities/OfferActivity.java
+++ app/src/main/java/com/project/technion/appark/activities/OfferActivity.java
@@ -22,31 +22,18 @@ public class OfferActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         double lat = getIntent().getDoubleExtra("lat", 0);
         double lng = getIntent().getDoubleExtra("lng", 0);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         addressLayer = findViewById(R.id.addressLayer);
+
+
+
+
         addressLayer.setOnClickListener(v -> {
-            /*
-            Geocoder gc = new Geocoder(getContext());
-            if(gc.isPresent()) {
-                List<Address> list = null;
-                try {
-                    list = gc.getFromLocation(offer.lat, offer.lng, 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                String found_address = list.get(0).getAddressLine(0);
-                String location = String.format(Locale.getDefault(), "geo:0,0?q=") + android.net.Uri.encode(String.format("%s@%f,%f", found_address, offer.lat, offer.lng), "UTF-8");
-                getContext().startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(location)));
-            }
-            */
-            String location = String.format(Locale.getDefault(), "geo:0,0?q=") + android.net.Uri.encode(String.format("%s@%f,%f", "", lat, lng), "UTF-8");
-            //String uri = "waze://?ll=40.761043, -73.980545&navigate=yes";
-            //String.format(Locale.ENGLISH, "geo:0,0?q=") + android.net.Uri.encode(String.format("%s@%f,%f", label, latitude, longitude), "UTF-8");
-            startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(location)));
+        String location = String.format(Locale.getDefault(), "geo:0,0?q=") + android.net.Uri.encode(String.format("%s@%f,%f", "", lat, lng), "UTF-8");
+        startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(location)));
         });
 
         FloatingActionButton fab = findViewById(R.id.fab);
