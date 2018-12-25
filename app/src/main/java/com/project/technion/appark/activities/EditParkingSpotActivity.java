81e7521854cd88d40adf5870319dee0e79cc69dd
Dor Kabareti <dor.kabareti@campus.technion.ac.il>
2018-12-25 20:02:27 +0200

    Created basic UI #65

---
+++ app/src/main/java/com/project/technion/appark/activities/EditParkingSpotActivity.java
@@ -0,0 +1,15 @@
+package com.project.technion.appark.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.technion.appark.R;
+
+public class EditParkingSpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_parking_spot);
+    }
+}
